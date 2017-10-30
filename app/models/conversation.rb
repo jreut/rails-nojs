c1f371caae8324ca1ab5d7503b5a1cfda01614f5
Jordan Ryan Reuter <jordan.reuter@scimedsolutions.com>
2017-10-29 22:06:21 -0400

    Include chat participants in header

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,9 @@
 class Conversation < ApplicationRecord
   has_many :messages
+  has_many :users, through: :messages
+
+  def participants
+    # OPTIMIZE: calculate uniqueness in the database
+    users.uniq.map(&:name)
+  end
 end
